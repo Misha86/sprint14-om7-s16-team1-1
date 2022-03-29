533f9aef27d225617db0113cf8e8fcfdadc1f601
Misha86 <mpolishchuk86@gmail.com>
2022-03-30 00:17:37 +0300

    add base.html

--- book/views.py
+++ book/views.py
@@ -4,4 +4,4 @@ from .models import Book
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'book_list.html', {'books': books})
+    return render(request, 'base.html', {'books': books})
