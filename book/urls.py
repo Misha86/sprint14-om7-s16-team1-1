09e3e5359f841aeca6223d1acc788101d347f24c
Misha86 <mpolishchuk86@gmail.com>
2022-03-30 14:49:49 +0300

    Created view for book

--- book/urls.py
+++ book/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('list/', views.book_list, name='book-list'),
+    path('<int:id>/', views.book, name='book'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template.response import TemplateResponse
 from .models import Book
 
@@ -9,4 +9,9 @@ def home_page(request):
 
 def book_list(request):
     return render(request, 'book_list.html', {'books': Book.get_all()})
+
+
+def book(request, id):
+    return render(request, 'book.html', {'book': get_object_or_404(Book, id=id)})
+
 
