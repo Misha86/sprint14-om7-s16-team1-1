be0c3b961f74968d7f99ba495eb18eab38b6ee5d
Misha86 <mpolishchuk86@gmail.com>
2022-04-02 15:47:55 +0300

    add filter for Users Violators

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,8 @@
+from django.db.models import F, Exists, OuterRef, Subquery
+from django.db.models.lookups import GreaterThan
 from django.shortcuts import render, get_object_or_404
+
+from order.models import Order
 from .models import CustomUser
 from library.utils import search_books, sort_by
 
@@ -9,6 +13,13 @@ def user_list(request):
 
 
 def user_books(request, id):
-    books = get_object_or_404(CustomUser, id=id).get_user_books()
-    return render(request, 'book_list.html', {'title': 'User Books',
+    user = get_object_or_404(CustomUser, id=id)
+    books = user.get_user_books()
+    return render(request, 'book_list.html', {'title': f'{user.get_full_name()} Books',
                                               'books': sort_by(request, search_books(request, books))})
+
+
+def users_violators(request):
+    users = CustomUser.objects.filter(Exists(Order.objects.filter(user=OuterRef('pk'), created_at__lt=F('end_at'))))
+    return render(request, 'user_list.html', {'users': users,
+                                              'title': 'Users Violators'})
