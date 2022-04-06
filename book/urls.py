738ca049b499f57a98723b1b0805d272c3dc65ae
Misha86 <mpolishchuk86@gmail.com>
2022-04-06 12:39:53 +0300

    added book-update

--- book/urls.py
+++ book/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('list/unordered/', views.unordered_books, name='unordered-books'),
     path('<int:id>/', views.book, name='book'),
     path('add/', views.book_form, name='book-add'),
+    path('<int:id>/update/', views.book_form, name='book-update'),
 ]
