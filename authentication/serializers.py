1e66c443b02bc089a1826440f4f17f3ba2c87148
Misha86 <mpolishchuk86@gmail.com>
2022-04-10 19:12:41 +0300

    order has done

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,15 +7,15 @@ from order.models import Order
 from .models import CustomUser
 
 
-def validator_password(password):
-    errors = dict()
-    try:
-        validate_password(password)
-    except exceptions.ValidationError as e:
-        errors['password'] = list(e.messages)
-
-    if errors:
-        raise serializers.ValidationError(errors)
+# def validator_password(password):
+#     errors = dict()
+#     try:
+#         validate_password(password)
+#     except exceptions.ValidationError as e:
+#         errors['password'] = list(e.messages)
+#
+#     if errors:
+#         raise serializers.ValidationError(errors)
 
 
 class CustomerHyperlink(serializers.HyperlinkedRelatedField):
@@ -38,55 +38,67 @@ class CustomerHyperlink(serializers.HyperlinkedRelatedField):
 
 class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='authentication:user-detail', lookup_field='pk')
-    role = serializers.CharField(source='get_role_display')
 
-    password = serializers.CharField(write_only=True, required=True, help_text='Leave empty if no change needed',
+    password = serializers.CharField(required=True, help_text='Leave empty if no change needed',
                                      style={'input_type': 'password', 'placeholder': 'Password'},
-                                     validators=[validator_password])
+                                     validators=[validate_password])
+
+    # write_only = True,
 
     class Meta:
         model = CustomUser
         fields = ['url', 'id', 'email', 'first_name', 'middle_name', 'last_name', 'role', 'is_active', 'password']
 
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data.get('password'))
-        return super().create(validated_data)
+        # validated_data['password'] = make_password(validated_data.get('password'))
+        # return super().create(validated_data)
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['role'] = instance.get_role_display()
+        return representation
 
 
 class CustomUserDetailSerializer(serializers.ModelSerializer):
     orders = CustomerHyperlink(many=True, read_only=True)
-    role = serializers.CharField(source='get_role_display')
-    new_password = serializers.CharField(write_only=True, allow_blank=True,
-                                         style={'input_type': 'password', 'placeholder': 'New Password'})
+    password = serializers.CharField(write_only=True, allow_blank=True, validators=[validate_password],
+                                     style={'input_type': 'password', 'placeholder': 'New Password'})
 
-    confirm_new_password = serializers.CharField(write_only=True, allow_blank=True,
-                                                 help_text='Leave empty if no change needed',
-                                                 style={'input_type': 'password',
-                                                        'placeholder': 'Confirmation Password'})
+    confirm_password = serializers.CharField(write_only=True, allow_blank=True,
+                                             help_text='Leave empty if no change needed',
+                                             style={'input_type': 'password',
+                                                    'placeholder': 'Confirmation Password'})
 
     class Meta:
         model = CustomUser
         fields = ['id', 'email', 'first_name', 'middle_name', 'last_name', 'role', 'is_active',
-                  'orders', 'new_password', 'confirm_new_password']
+                  'orders', 'password', 'confirm_password']
 
     def validate(self, data):
-        new_password = data.get('new_password')
-        confirm_new_password = data.get('confirm_new_password')
-        if new_password and confirm_new_password:
-            if new_password != confirm_new_password:
-                raise serializers.ValidationError("Password confirmation does not match.")
-            else:
-                validator_password(new_password)
-        elif any([new_password, confirm_new_password]):
-            raise serializers.ValidationError("Didn`t enter the password confirmation.")
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+        if password and confirm_password:
+            if password != confirm_password:
+                raise serializers.ValidationError({"password": "Password confirmation does not match."})
+            # else:
+            #     validator_password(new_password)
+        elif any([password, confirm_password]):
+            raise serializers.ValidationError({"confirm_password": "Didn`t enter the password confirmation."})
 
         return super().validate(data)
 
     def update(self, instance, validated_data):
-        confirm_new_password = validated_data.pop('confirm_new_password')
-        if confirm_new_password:
-            validated_data['password'] = make_password(confirm_new_password)
+        confirm_password = validated_data.pop('confirm_password')
+        if confirm_password:
+            validated_data['password'] = make_password(confirm_password)
         return super().update(instance, validated_data)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['role'] = instance.get_role_display()
+        return representation
 
 
 class UserOrderDetailSerializer(serializers.HyperlinkedModelSerializer):
