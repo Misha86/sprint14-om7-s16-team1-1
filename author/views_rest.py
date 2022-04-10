1c429e1c36582be23b16cd2c0f8b5c446c789fb3
Maestro28 <bohdan.rybak28@gmail.com>
2022-04-10 15:17:01 +0300

    author simple rest completed

---
+++ author/views_rest.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import viewsets, generics
+from .models import Author
+from .serializers import AuthorSerializer
+
+
+#
+
+
+class AuthorGenerics(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorDetailGenerics(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
